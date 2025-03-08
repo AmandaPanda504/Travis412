a323b47dfe983d1072ef5be47f2d829231ea4d6d
asjames1 <asjames1@uno.edu>
2025-03-08 13:12:58 -0500

    5-3

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,36 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	
+	
+	/*@Test
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(5, 3), 2);
+	}*/
+	
+	
+	
+	@Test
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(5, 3), 2);
+	}
+	
+	/*
+	
+	
+	@Test
+	public void testMultiple() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(8,2), 16);
+	}
+	
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(6, 3), 2);
+	}
+	
+	*/
 }
